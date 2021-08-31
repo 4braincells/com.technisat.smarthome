94502e4bc895d7d8dcc4b9f2b92d5a445d2b18f4
Kaalus <KyleTaylorParkins@users.noreply.github.com>
2021-08-31 09:13:55 +0200

    Version 1.3.5 - Fix dimmer onoff capability

--- drivers/wall-dimmer/device.js
+++ drivers/wall-dimmer/device.js
@@ -4,12 +4,12 @@ const { ZwaveDevice } = require('homey-meshdriver');
 
 class Dimmer extends ZwaveDevice {
 
-	onMeshInit() {
+	async onMeshInit() {
 		this.enableDebug();
 
 		this.log('Dimmer has been inited');
 
-		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
 		this.registerCapability('dim', 'SWITCH_MULTILEVEL')
 		this.registerCapability('measure_power', 'METER');
 		this.registerCapability('meter_power', 'METER');
