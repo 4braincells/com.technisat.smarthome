9be7a743fec1f47b8caa660f2b382bf000ae4670
Christian Roring <hallo@burostoeko.nl>
2021-12-07 14:26:15 +0100

    Added the Multichannel endpoint for the 2 switches

--- drivers/double-wall-switch/device.js
+++ drivers/double-wall-switch/device.js
@@ -10,7 +10,10 @@ class DoubleSwitch extends ZwaveDevice {
 
 		this.log('DoubleSwitch has been inited');
 
-		this.registerCapability('onoff', 'SWITCH_BINARY');
+		// Based on the docs: Supported association groups - Connection 1, Connection 2.
+    		this.registerCapability('onoff.output1', 'BASIC_SET', { multiChannelNodeId: 1 });
+    		this.registerCapability('onoff.output2', 'BASIC_SET', { multiChannelNodeId: 2 });
+		
 		// Disable these command classes during testing for the multi channel
 		this.registerCapability('meter_power', 'METER');
 		this.registerCapability('measure_power', 'METER');
